7bc0be15d4d0bf5789a1ba2de1b713e084ac75dd
Sai Narendra <tannirusainarendra81@gmail.com>
2025-09-20 00:16:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstChar(text) {
   // your code here
+  if (text.length <= 1) return " ";
+	for (let index = 0; index < text.length; index++) {
+		if (text[index] !== " ") return text[index];
+	}
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
-alert(firstChar(text));
+console.log(firstChar(""));
